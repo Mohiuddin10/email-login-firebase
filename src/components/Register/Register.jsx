37a72d223b419c87ab49be19b4e79f1f2e30ab0d
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-11-18 01:02:30 +0600

    show and hide password icon added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,21 +1,25 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase.config/firebase.config";
 import { useState } from "react";
+import { FcCancel, FcApproval } from "react-icons/fc";
 
 const Register = () => {
     const [registerError, setRegisterError] = useState('');
     const [success, setSuccess] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+
     const handleRegister = e => {
         e.preventDefault();
         console.log('btn clicked');
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
-        if (password.length < 6){
+        if (password.length < 6) {
             setRegisterError('password is less then 6 charecter')
             return
         }
-        else if (!/[A-Z]/.test(password)){
+        else if (!/[A-Z]/.test(password)) {
             setRegisterError('you should have at least one upper case charecter');
             return;
         }
@@ -24,9 +28,11 @@ const Register = () => {
         // ste success message reset 
         setSuccess('')
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {console.log(result.user)
-        setSuccess('User created successfully')})
-        .catch(error => setRegisterError(error.message))
+            .then(result => {
+                console.log(result.user)
+                setSuccess('User created successfully')
+            })
+            .catch(error => setRegisterError(error.message))
     }
     return (
         <>
@@ -34,7 +40,14 @@ const Register = () => {
                 <h2 className="text-xl font-extrabold">Register here</h2>
                 <input className="m-4 p-2 w-3/4" type="email" placeholder="email" name="email" id="register-email" />
                 <br />
-                <input className="m-4 p-2 w-3/4" type="password" placeholder="password" name="password" id="register-password" />
+                <div className=" flex flex-row">
+                    <input className="m-4 p-2 w-3/4" type={showPassword ? 'text' : 'password'} placeholder="password" name="password" id="register-password" />
+                    <span className="mt-6" onClick={() => setShowPassword(!showPassword)}>
+                        {
+                            showPassword ? <FcApproval /> : <FcCancel />
+                        }
+                    </span>
+                </div>
                 <br />
                 <button className="btn btn-success">Submit</button>
             </form>
